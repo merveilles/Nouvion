7405935f2618c83a584e55ab86439ce06b0aa425
Devine Lu Linvega <VillaMoirai@VillaMoirai.local>
2015-03-30 17:00:36 -0400

    Example module

---
+++ module.roll.rb
@@ -0,0 +1,17 @@
+class Answer
+
+	# Available: moduleName,methodName,username,message
+
+	def dice
+
+		diceValue = @message.split(" ")[2].to_i
+
+		if diceValue != 0
+			return Hash["text" => "Rolling a "+diceValue.to_s+" sided dice, the result is "+(rand(diceValue)+1).to_s+"."]
+		end
+
+	  	return Hash["text" => "Rolling a 6 sided dice, the result is "+(rand(6)+1).to_s+".("+test+")"]
+
+	end
+	
+end
