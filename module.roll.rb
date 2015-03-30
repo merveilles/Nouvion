66ccefdde230545403405c53212b98427a5ace95
Andrew Matecha <amatecha@gmail.com>
2015-03-30 15:13:30 -0700

    Quick test… Improved check for diceValue ;)

--- module.roll.rb
+++ module.roll.rb
@@ -6,7 +6,7 @@ class Answer
 
 		diceValue = @message.split(" ")[2].to_i
 
-		if diceValue > 100000000000
+		if diceValue > 100000000000 || diceValue < 1
 			return Hash["text" => "Mhm, grow up."]
 		end
 
