29554b1fcb2db67bff2ee54ed2ca53b77f96ccbc
Marshall Bowers <elliott.codes@gmail.com>
2015-03-31 19:51:33 -0400

    Add tests for goat and lorem

---
+++ test/test_goat.rb
@@ -0,0 +1,46 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'coveralls'
+Coveralls.wear!
+
+require 'minitest/autorun'
+require 'memory'
+require_relative '../answer'
+require_relative '../module.goat'
+
+class TestGoat < Minitest::Test
+
+    def test_goat_goat
+
+        answer = Answer.new("goat", "goat", "maxdeviant", "ludivine goat", "theartificiallounge")
+
+        goat = answer.goat()
+
+        assert_equal "What about goats?", goat["text"]
+
+    end
+
+    def test_goat_tower
+
+        answer = Answer.new("goat", "tower", "maxdeviant", "ludivine goat tower", "theartificiallounge")
+
+        tower = answer.tower()
+
+        assert_equal true, (tower.instance_of? Hash)
+        assert_equal true, (tower["text"].instance_of? String)
+
+    end
+
+    def test_goat_thrower
+
+        answer = Answer.new("goat", "thrower", "maxdeviant", "ludivine goat thrower", "theartificiallounge")
+
+        thrower = answer.thrower()
+
+        assert_equal true, (thrower.instance_of? Hash)
+        assert_equal true, (thrower["text"].instance_of? String)
+
+    end
+
+end
