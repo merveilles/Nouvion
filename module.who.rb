84f7cccd5870c20be47974dc183435a0fea42539
Devine Lu Linvega <VillaMoirai@VillaMoirai.local>
2015-03-30 20:22:32 -0400

    Added the who module

---
+++ module.who.rb
@@ -0,0 +1,22 @@
+class Answer
+
+	# Available: moduleName,methodName,username,message
+
+	def is
+
+		username = @message.split(" ")[2]
+
+		vessels = {}
+		vessels["aliceffekt"] = "many witches"
+		vessels["theneko"] = "a cat"
+		vessels["3wm"] = "a single wise man"
+		vessels["maxdeviant"] = "actually Marshall"
+		vessels["cancel"] = "someone very clever"
+
+		if vessels[username] then return Hash["text" => "*@#{username}* is "+vessels[username]+"."] end
+
+		return Hash["text" => "I do not know *#{username}*."]
+
+	end
+	
+end
