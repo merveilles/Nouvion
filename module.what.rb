95e23ac5bbb892c1c507b981b9249641c19027e8
Marshall Bowers <elliott.codes@gmail.com>
2015-03-31 10:23:41 -0400

    Add Twitter links for "what is"

--- module.what.rb
+++ module.what.rb
@@ -1,26 +1,30 @@
 class Answer
 
-	# Available: moduleName,methodName,username,message
+    # Available: moduleName,methodName,username,message
 
-	def time
-		return Hash["text" => "The XXIIV server time is "+Time.new.strftime("%H:%M:%S")+"."]
-	end
+    def time
+        return Hash["text" => "The XXIIVV server time is "+Time.new.strftime("%H:%M:%S")+"."]
+    end
 
-	def day
-		return Hash["text" => "We are the "+Time.new.strftime("%B %d, %Y")+"."]
-	end
+    def day
+        return Hash["text" => "We are the "+Time.new.strftime("%B %d, %Y")+"."]
+    end
 
-	def is
-		@memory.connect()
-		query = @message.sub("what is ","").strip
-	    thoughts = @memory.load(query).shuffle
+    def is
+        @memory.connect()
+        query = @message.sub("what is ","").strip
+        thoughts = @memory.load(query).shuffle
 
-		if thoughts.length > 1
-	      return Hash["text" => "*"+thoughts[0][1]+"* is *"+thoughts[0][2]+"* and *"+thoughts[1][2]+"*."]
-	    elsif thoughts.length > 0
-	      return Hash["text" => "*"+thoughts[0][1]+"* is *"+thoughts[0][2]+"*."]
-	    end
-	    return Hash["text" => "I don't know.."]
-	end
-	
+        if @message.include? "twitter"
+            return Hash["text" => "*#{thoughts[0][1]}* is https://twitter.com/#{thoughts[0][2]}."]
+        end
+
+        if thoughts.length > 1
+          return Hash["text" => "*"+thoughts[0][1]+"* is *"+thoughts[0][2]+"* and *"+thoughts[1][2]+"*."]
+        elsif thoughts.length > 0
+          return Hash["text" => "*"+thoughts[0][1]+"* is *"+thoughts[0][2]+"*."]
+        end
+        return Hash["text" => "I don't know.."]
+    end
+
 end
