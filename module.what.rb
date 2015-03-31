b9d8de35aa513833895aec13b409aee50084037f
Devine Lu Linvega <VillaMoirai@VillaMoirai.local>
2015-03-31 15:29:36 -0400

    Improved the recognition of the what module

--- module.what.rb
+++ module.what.rb
@@ -12,8 +12,21 @@ class Answer
 
     def is
         @memory.connect()
-        query = @message.sub("what is ","").strip
+        query = @message.sub("what is","").strip
         thoughts = @memory.load(query).shuffle
+
+        # Any message
+
+        if thoughts.length < 1 then return Hash["text" => "I don't know"] end
+
+        # Look for a message by the user
+
+        thoughts.each do |known|
+            if known[0] != @username then next end
+            return Hash["text" => "*"+known[1]+"* is *"+known[2].sub("my","your").strip+"*."]
+        end
+
+        # Look for any message
 
         if @message.include?("twitter") && thoughts.length > 0
             username = thoughts[0][2]
@@ -25,6 +38,7 @@ class Answer
         elsif thoughts.length > 0
           return Hash["text" => "*"+thoughts[0][1]+"* is *"+thoughts[0][2]+"*."]
         end
+        
         return Hash["text" => "I don't know.."]
     end
 
