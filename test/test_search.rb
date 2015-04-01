916c296c805da64c87755ab1057f59f82e8a058b
Marshall Bowers <elliott.codes@gmail.com>
2015-03-31 23:02:29 -0400

    Add tests for search

---
+++ test/test_search.rb
@@ -0,0 +1,34 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'coveralls'
+Coveralls.wear!
+
+require 'minitest/autorun'
+require 'memory'
+require_relative '../answer'
+require_relative '../module.search'
+
+class TestSearch < Minitest::Test
+
+    def test_search_google
+
+        answer = Answer.new("search", "google", "maxdeviant", "ludivine google cat fact", "theartificiallounge")
+
+        google = answer.google()
+
+        assert_equal "http://www.google.com/search?q=cat%20fact&btnI", google["text"]
+
+    end
+
+    def test_search_duckduckgo
+
+        answer = Answer.new("search", "duckduckgo", "maxdeviant", "ludivine search duckduckgo cat fact", "theartificiallounge")
+
+        duckduckgo = answer.duckduckgo()
+
+        assert_equal "https://duckduckgo.com/?q=!ducky+duckduckgo%20cat%20fact+%s", duckduckgo["text"]
+
+    end
+
+end
