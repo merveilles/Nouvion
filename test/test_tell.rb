2134cae94ddea797e111f2eed9652297725dde9a
Marshall Bowers <elliott.codes@gmail.com>
2015-03-31 20:03:10 -0400

    Add tests for tell

---
+++ test/test_tell.rb
@@ -0,0 +1,39 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+require 'coveralls'
+Coveralls.wear!
+
+require 'minitest/autorun'
+require 'memory'
+require_relative '../answer'
+require_relative '../module.tell'
+
+class TestTell < Minitest::Test
+
+    def test_tell_tell
+
+        answer = Answer.new("tell", "tell", "maxdeviant", "ludivine tell", "theartificiallounge")
+
+        tell = answer.tell()
+
+        assert_equal "I don't know what to tell you.", tell["text"]
+
+        messages = [
+            "ludivine tell me something dark",
+            "ludivine rules",
+            "ludivine manifesto"
+        ]
+
+        messages.each do |message|
+            answer = Answer.new("tell", "tell", "maxdeviant", message, "theartificiallounge")
+
+            tell = answer.tell()
+
+            assert_equal true, (tell.instance_of? Hash)
+            assert_equal true, (tell["text"].instance_of? String)
+        end
+
+    end
+
+end
