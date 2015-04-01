2bb78c9f1b10ec52a4f5ac48285e12ad4f4b742b
Marshall Bowers <elliott.codes@gmail.com>
2015-03-31 22:53:12 -0400

    Clean up what module

--- module.what.rb
+++ module.what.rb
@@ -3,16 +3,20 @@ class Answer
     # Available: moduleName,methodName,username,message
 
     def time
-        return Hash["text" => "The XXIIVV server time is "+Time.new.strftime("%H:%M:%S")+"."]
+        time = Time.new.strftime("%H:%M:%S")
+
+        return Hash["text" => "The XXIIVV server time is #{time}"]
     end
 
     def day
-        return Hash["text" => "We are the "+Time.new.strftime("%B %d, %Y")+"."]
+        day = Time.new.strftime("%B %d, %Y")
+
+        return Hash["text" => "We are the #{day}."]
     end
 
     def is
         @memory.connect()
-        query = @message.sub("what is","").strip
+        query = @message.sub("what is", "").strip
         thoughts = @memory.load(query).shuffle
 
         # Any message
@@ -23,20 +27,22 @@ class Answer
 
         thoughts.each do |known|
             if known[0] != @username then next end
-            return Hash["text" => "*"+known[1]+"* is *"+known[2].sub("my","your").strip+"*."]
+
+            return Hash["text" => "*" + known[1] + "* is *" + known[2].sub("my", "your").strip + "*."]
         end
 
         # Look for any message
 
         if @message.include?("twitter") && thoughts.length > 0
             username = thoughts[0][2]
+
             return Hash["text" => "*#{thoughts[0][1]}* is <https://twitter.com/#{username}|@#{username}>."]
         end
 
         if thoughts.length > 1
-          return Hash["text" => "*"+thoughts[0][1]+"* is *"+thoughts[0][2]+"* and *"+thoughts[1][2]+"*."]
+            return Hash["text" => "*#{thoughts[0][1]}* is *#{thoughts[0][2]}* and *#{thoughts[1][2]}*."]
         elsif thoughts.length > 0
-          return Hash["text" => "*"+thoughts[0][1]+"* is *"+thoughts[0][2]+"*."]
+            return Hash["text" => "*#{thoughts[0][1]}* is *#{thoughts[0][2]}*."]
         end
 
         return Hash["text" => "I don't know.."]
