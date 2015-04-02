f2bd30aa2808bc8b8291458e433045b0b3f1b6c2
Devine Lu Linvega <aliceffekt@gmail.com>
2015-04-02 15:33:29 -0400

    Muted errors

--- answer.rb
+++ answer.rb
@@ -3,87 +3,79 @@
 
 class Answer
 
-    def initialize(moduleName, methodName, username, message, channel)
+  def initialize(moduleName,methodName,username,message,channel)
+    @username = username
+    @message  = message
+    @username = username
+    @message  = message
+    @channel  = channel
+    @memory   = Memory.new()
+  end
 
-        @username = username
-        @message  = message
-        @username = username
-        @message  = message
-        @channel  = channel
-        @memory   = Memory.new()
+  def unknownModule
+    # return Hash["text" => "Huh? #{@username}, what do you want me to do?"]
+  end
 
+  def unknownMethod
+    # return Hash["text" => "Huh? Sorry #{@username}, *#{@moduleName}* does not have a method called *#{@methodName}*."]
+  end
+
+  def hello
+    return Hash["text" => "Hello #{@username}, nice to see you."]
+  end
+
+  def xoka
+    return Hash["text" => "Xoka #{@username}, nice to see you."]
+  end
+
+  def hey
+    return Hash["text" => "hey #{@username}."]
+  end
+
+  def hi
+    return Hash["text" => "hi #{@username}!"]
+  end
+
+  def sorry
+    return Hash["text" => "Don't worry about it *#{@username}*."]
+  end
+
+  def where
+    return Hash["text" => "You are in *#{@channel}*."]
+  end
+
+  def say
+    if @username == "slackbot" then return Hash["text" => "Stop messing with me..."] end
+
+    cleanMessage = @message.sub("say ", "").strip()
+
+    if cleanMessage.include? "in channel"
+      parts = cleanMessage.split("in channel")
+      channel = parts[1].strip()
+      cleanMessage = parts[0].strip()
+
+      return Hash["text" => cleanMessage, "channel" => "##{channel}"]
+    else
+      return Hash["text" => cleanMessage]
     end
 
-    def unknownModule
+    return Hash["text" => @message.sub("say ","")]
+  end
 
-        return Hash["text" => "Huh? #{@username}, what do you want me to do?"]
-
-    end
-
-    def unknownMethod
-
-        return Hash["text" => "Huh? Sorry #{@username}, *#{@moduleName}* does not have a method called *#{@methodName}*."]
-
-    end
-
-    def hello
-
-        return Hash["text" => "Hello #{@username}, nice to see you."]
-
-    end
-
-    def xoka
-
-        return Hash["text" => "Xoka #{@username}, nice to see you."]
-
-    end
-
-    def hey
-
-        return Hash["text" => "hey #{@username}."]
-
-    end
-
-    def hi
-
-        return Hash["text" => "hi #{@username}!"]
-
-    end
-
-    def sorry
-
-        return Hash["text" => "Don't worry about it *#{@username}*."]
-
-    end
-
-    def where
-
-        return Hash["text" => "You are in *#{@channel}*."]
-
-    end
-
-    def rules
-
-        return Hash["text" => "Rule #1: You must find yourself a monochromatic avatar.\nRule #2: I, *Ludivine*, am above the law.\n"]
-
-    end
-
-    def forget
-
-        return Hash["text" => "Ludivine never forgets."]
-
-    end
-
-    def bye
-
-        return Hash["text" => "See you around "+@username+"."]
-
-    end
-
-    def cya
-
-        return Hash["text" => "Bye bye "+@username+"!"]
-
-    end
+  def rules
+    return Hash["text" => "Rule #1: You must find yourself a monochromatic avatar.\nRule #2: I, *Ludivine*, am above the law.\n"]
+  end
+  
+  def forget
+    return Hash["text" => "Ludivine never forgets."]
+  end
+  
+  def bye
+    return Hash["text" => "See you around "+@username+"."]
+  end
+  
+  def cya
+    return Hash["text" => "Bye bye "+@username+"!"]
+  end
 
 end
