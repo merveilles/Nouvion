9b5f440d0a916ca3baf2e5d0502fe706a7382e89
Devine Lu Linvega <aliceffekt@gmail.com>
2015-04-02 14:56:05 -0400

    Allowed access to talk across channels.

--- module.say.rb
+++ module.say.rb
@@ -6,32 +6,21 @@ require 'json'
 
 class Answer
 
-    def say
+	def say
+		if @username == "slackbot" then return Hash["text" => "Stop messing with me..."] end
 
-        if @username == "slackbot" then return Hash["text" => "Stop messing with me..."] end
+		cleanMessage = @message.sub("say ", "").strip()
 
-        cleanMessage = @message.sub("say ", "").strip()
+		if cleanMessage.include? "in channel"
+			parts = cleanMessage.split("in channel")
+			channel = parts[1].strip()
+			cleanMessage = parts[0].strip()
+			sayInChannel("#"+channel,cleanMessage)
+		else
+			return Hash["text" => cleanMessage]
+		end
 
-        if cleanMessage.include? "in channel"
-            parts = cleanMessage.split("in channel")
-            channel = parts[1].strip()
-            cleanMessage = parts[0].strip()
-
-            url = URI.parse($webhookIncoming)
-            params = {:payload => {:text => cleanMessage, :channel => "##{channel}"}}
-            res = Net::HTTP.post_form(url, params)
-
-            case res
-            when Net::HTTPSuccess, Net::HTTPRedirection
-                # all good
-            else
-                return Hash["text" => "Couldn't make it happen, sorry."]
-            end
-
-        else
-            return Hash["text" => cleanMessage]
-        end
-
-    end
+		return Hash["text" => cleanMessage]
+	end
 
 end
