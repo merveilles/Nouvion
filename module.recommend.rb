66dab5bb89549ffb216417e2c8eb01913f7ccd10
Devine Lu Linvega <aliceffekt@gmail.com>
2015-04-02 15:24:02 -0400

    Connected the recommend module to the memory

--- module.recommend.rb
+++ module.recommend.rb
@@ -5,27 +5,17 @@ class Answer
 
     def recommend
 
-        return Hash["text" => "What would you like me to recommend?"]
+        topic = @message.gsub("recommend","").strip
 
-    end
+        @memory.connect()
+        thoughts = @memory.load(topic)
 
-    def alcohol
+        thoughts.each do |known|
+            if !known[2].include?(topic) then next end
+            return Hash["text" => "How about *"+known[1]+"*?"]
+        end
 
-        choices = [
-            "Any Beer", "Any Ale", "Barelywine",
-            "Bitter Ale", "Brown Ale", "Mild Ale",
-            "Old Ale", "Pale Ale", "Pale Ale", "Any scotch",
-            "Any Vodka", "Any Wiskey", "Any Lager", "Pale lager",
-            "Bock Lager", "Pilsener Lager", "Schwarzbier Lager",
-            "holy shit Japan is awesome Im 19 and can walk into a store and buy all the alcohol I want",
-            "I was really fucking drunk while coding this",
-            "Cider beer", "Icariine Liquor", "Mead", "Palm Wine",
-            "Sake", "Definitely Sake", "You should find sake and drink it."
-            "Wine... gross", "Fruit wine", "Table Wine", "Sparking Wine", "Champagne"
-            "Any cocktail", "Force aliceffekt to brew his own beer and send it to you."
-        ]
-
-        return Hash["text" => choices.shuffle[0]]
+        return Hash["text" => "What the hell is *"+known[2]+"*?"]
 
     end
 
