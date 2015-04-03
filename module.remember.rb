532feb3b914e2010d203c00d668af0471a97ade5
xoorath <jared@xoorath.com>
2015-04-03 16:45:55 +0000

    adding additional terms to the remember module. just makes it a bit nicer to use.

--- module.remember.rb
+++ module.remember.rb
@@ -7,23 +7,39 @@ class Answer
         if @message.split(" ")[1] == "that"
             @message = @message.sub("that", "").strip
         end
-
-        if @message.include?(" is ") && @message.split(" ").length > 3
-            @message = @message.sub("remember", "").strip
-            memoryName = @message.split(" is ")[0].to_s.strip
-            memoryValue = @message.split(" is ")[1].to_s.strip
-
-            if memoryValue.include?("http")
-                return Hash["text" => "I cannot remember URLs."]
-            elsif memoryName != "" && memoryValue != ""
-                @memory.save(@username, memoryName, memoryValue)
-                return Hash["text" => "I will remember that *#{memoryName}* is *#{memoryValue}*."]
-            end
-
+        
+        @message = @message.sub("remember", "").strip
+        
+        termSplit = termToSplit(@message)
+        name = termSplit[0]
+        value = termSplit[1]
+        term = termSplit[2]
+        
+        if value.include?("http")
+            return Hash["text" => "I cannot remember URLs."]
         end
-
+        
+        if name != "" && value != "" && term != ""
+            @memory.save(@username, name, value)
+            return Hash["text" => "I will remember that *#{name}*" + term + "*#{value}*."]
+        end
+        
         return Hash["text" => "What do you want me to remember #{@username}?"]
 
+    end
+    
+    def termToSplit(msg)
+        terms = ["is", "are", "have", "has"]
+        if msg.split(" ").length > 2
+            for term in terms
+                name = msg.split(" " + term + " ")[0].to_s.strip
+                value = msg.split(" " + term + " ")[1].to_s.strip
+                if name != "" && value != ""
+                    return [name, value, " " + term + " "]
+                end
+            end
+        end
+        return ["", "", ""]
     end
 
 end
