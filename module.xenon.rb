8b3e4d4fb3e08c48365a3100487bc7c7dc3db1f4
Titouan Millet <titouan.millet@gmail.com>
2015-04-04 07:10:44 +0200

    [+] xenon module (money stuff) [*] little modif in some seeds of color module

---
+++ module.xenon.rb
@@ -0,0 +1,65 @@
+class Answer
+
+    def xenon
+
+        return "If you own an 'account', you can `give` money to another user - or request to see the `wallets`."
+
+    end
+
+    def account
+
+        @memory.connect()
+        thoughts = @memory.load("wallet ")
+
+        thoughts.each do |known|
+            if known[0] != "ludivine" then next end
+            if known[1] == "wallet "+@username then return "#{@username}, you have currently "+known[2]+ " xenons on your account." end
+        end
+
+        @memory.save("ludivine","wallet #{@username}","10")
+        return "#{@username} subscribes an account with a credit of 10 xenons."
+
+    end
+
+    def wallets
+
+        @memory.connect()
+        thoughts = @memory.load("wallet ")
+        wallets = ""
+        thoughts.each do |known|
+            if known[0] != "ludivine" then next end
+            if known[1].split(" ")[0] != "wallet" then next end
+            wallets += "*"+known[1].sub("wallet","")+"* "+known[2]+"xens\n"
+        end
+        return "Your wallets are:\n#{wallets}"
+
+    end
+
+    def give
+
+        target = @message.sub("give","").split(" ")[1]
+
+        gift = @message.sub("give","").split(" ")[2]
+        if gift.to_i.to_s != gift then return "Please specify a correct amount of xenons." end
+
+        accountsXenons = ["",""] # accountsXenons[0] for sender xenons, accountsXenons[1] for receiver xenons
+        @memory.connect()
+        thoughts = @memory.load("wallet ")
+        thoughts.each do |known|
+            if known[0] != "ludivine" then next end
+            if known[1] == "wallet "+@username then accountsXenons[0] = known[2] end
+            if known[1] == "wallet "+target then accountsXenons[1] = known[2] end
+            if accountsXenons[0] !="" && accountsXenons[1] !="" then break end
+        end
+
+        if accountsXenons[0] == "" then return "#{@username}, you don't have an account thus can't give money." end
+        if accountsXenons[1] == "" then return "#{target} doesn't have an account to send money on." end
+        if accountsXenons[0].to_i - gift.to_i < 0 then return "#{@username}, you don't have enough xenons on your account." end
+
+        @memory.save("ludivine","wallet #{target}",(accountsXenons[1].to_i + gift.to_i).to_s)
+        @memory.save("ludivine","wallet #{@username}",(accountsXenons[0].to_i - gift.to_i).to_s)
+        return "Transfert successful.\nCurrent #{@username} account: "+ (accountsXenons[0].to_i - gift.to_i).to_s+" xenons.\nCurrent #{target} account: "+ (accountsXenons[1].to_i + gift.to_i).to_s+" xenons."
+
+    end
+
+end
---
+++ test/test_xenon.rb
@@ -0,0 +1,37 @@
+# Require the main class
+require '../answer'
+require './memory'
+
+# Require any modules you might need
+require('../module.xenon')
+
+# Set up the payload
+
+class MemoryTest < Memory
+
+    def connect
+
+    end
+
+    def load(query)
+
+        return [
+            [ "ludivine", "wallet aliceffekt", "10" ],
+            [ "ludivine", "wallet greylion", "8" ],
+            [ "ludivine", "wallet maxdeviant", "5" ],
+            [ "ludivine", "wallet jean", "999" ]
+        ]
+
+    end
+
+    def save(stuffA, stuffB, stuffC)
+
+    end
+
+end
+
+answer = Answer.new("xenon", "give", "jean", "xenon give aliceffekt 5", "theartificiallounge",MemoryTest.new())
+
+# Output the result
+puts answer.give()
+
