f7718c6d46e96ca1800b61435f077bcfa52c4220
Luc Tremblay <luc@tons-of-slack.net>
2015-04-05 13:45:17 -0400

    restored lost changes to flip

--- test/test_emote.rb
+++ test/test_emote.rb
@@ -32,12 +32,6 @@ class TestEmote < Minitest::Test
 
     end
 
-    def test_emote_flip
-
-        answer = Answer.new("emote", "flip", "ragekit", "emote flip test", "theartificiallounge")
-
-    end
-
     def test_cat_fact
 
         answer = Answer.new("cat", "fact", "maxdeviant", "cat fact", "theartificiallounge")
@@ -48,4 +42,46 @@ class TestEmote < Minitest::Test
 
     end
 
+    def test_flip
+      
+        # mock memory
+        memory = FlipMemoryTest.new()
+        
+        # flip once...
+        answer = Answer.new("emote", "flip", "strstr", "emote flip", "theartificiallounge", memory)
+        emote1 = answer.flip()
+        assert_equal true, answer.flippingEmojis.include?(emote1)
+
+        # unflip
+        answer = Answer.new("emote", "flip", "strstr", "emote flip", "theartificiallounge", memory)
+        emote2 = answer.flip()
+        assert_equal true, answer.unflippingEmojis.include?(emote2)
+        
+        # flip again
+        answer = Answer.new("emote", "flip", "strstr", "emote flip", "theartificiallounge",memory)
+        emote3 = answer.flip()
+        assert_equal true, answer.flippingEmojis.include?(emote3)
+        
+        # and confirm unflip
+        answer = Answer.new("emote", "flip", "strstr", "emote flip", "theartificiallounge",memory)
+        emote4 = answer.flip()
+        assert_equal true, answer.unflippingEmojis.include?(emote4)
+    end
+end
+
+class FlipMemoryTest < Memory 
+  
+    def initialize
+      @mem = [["ludivine", "table flipped", "no"], ["ludivine", "other thing", "potato"]]
+    end
+
+    def load(query)
+
+        return @mem
+
+    end
+    
+    def save(owner, key, value)
+        @mem[0][2] = value
+    end
 end
