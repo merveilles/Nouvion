78489eacf641df34ac1d03b7db86aa5d50fe4545
Renaud Bédard <renaud.b@capybaragames.com>
2015-04-06 13:56:57 -0400

    Fixed to use a Ruby 1.8 equivalent instead of keep_if

--- module.list.rb
+++ module.list.rb
@@ -9,10 +9,10 @@ class Answer
 
         @memory.connect()
         thoughts = @memory.load(topic)
-        thoughts.keep_if {|x| x[2].include?(topic) }
+        matches = thoughts.select {|x| x[2].include?(topic) }
 
-        if thoughts.size() > 0 then
-            return "I know #{thoughts.size()} kind#{thoughts.size() == 1 ? "" : "s"} of *#{topic}* : *#{thoughts.map { |x| x[1] }.join("*, *")}*."
+        if matches.size() > 0 then
+            return "I know #{matches.size()} kind#{matches.size() == 1 ? "" : "s"} of *#{topic}* : *#{matches.map { |x| x[1] }.join("*, *")}*."
         end
 
         return "I don't know any *#{topic}*."
