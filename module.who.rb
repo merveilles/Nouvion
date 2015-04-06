0f30c43420e63a1f5dc6a763a77f9d850aa67651
Devine Lu Linvega <aliceffekt@gmail.com>
2015-04-06 10:25:20 -0400

    Added collaborations and twitter support to the who is module.

--- module.who.rb
+++ module.who.rb
@@ -9,13 +9,26 @@ class Answer
         @memory.connect()
         thoughts = @memory.load(username).shuffle
 
+        definition = ""
+        twitter = ""
+        collaborations = 0
+        collaborationsString = ""
+
         thoughts.each do |known|
-            if known[1] != username then next end
-            return "*"+username+"* is "+known[2]+"."
+
+            # Definition
+            if known[1] == username then definition = known[2] end
+            if known[0] == username && known[1] == "my twitter" then twitter = "And <https://twitter.com/"+known[2]+"|@"+known[2]+"> on Twitter. " end
+            if known[0] == username then collaborations += 1 end
         end
 
-        return "I do not know *#{username}*."
+        if collaborations > 0
+            collaborationsString = "*"+username+"* has added *#{collaborations} entries* to my memory."
+        end
 
+        if definition == "" then return "I do not know *#{username}*." end
+
+        return "*"+username+"* is "+definition+". "+twitter+collaborationsString
     end
 
     def am
