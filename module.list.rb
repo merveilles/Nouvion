0c06abe92fb2d8b11dedc398a0edb97c24a2eeeb
Renaud Bédard <renaud.b@capybaragames.com>
2015-04-07 10:16:06 -0400

    Whoops, Ruby 1.8 isn't happy with linefeeds

--- module.list.rb
+++ module.list.rb
@@ -13,9 +13,7 @@ class Answer
         @memory.connect()
         thoughts = @memory.load(topic)
 
-        matches = thoughts
-            .select {|x| x[2].include?(topic) }.map { |x| x[1] }
-            .uniq
+        matches = thoughts.select {|x| x[2].include?(topic) }.map { |x| x[1] }.uniq
 
         if matches.size() == 1 then
             return "The only *#{topic}* I know is _#{matches[0]}_."
@@ -32,10 +30,7 @@ class Answer
         @memory.connect()
         thoughts = @memory.load("")
 
-        matches = thoughts
-            .select.with_index { |x,i| thoughts.index.with_index { |y,j| y[2] == x[2] && j != i } != nil }
-            .map { |x| x[2] }
-            .uniq
+        matches = thoughts.select.with_index { |x,i| thoughts.index.with_index { |y,j| y[2] == x[2] && j != i } != nil }.map { |x| x[2] }.uniq
 
         if matches.size() > 0 then
             return "There are #{matches.size()} listable topics : _#{matches.join("_, _")}_."
