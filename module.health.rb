3594c83e1f170946a750186def71a7bffcb77a9c
Marshall Bowers <elliott.codes@gmail.com>
2015-04-07 19:33:58 -0400

    Add health module

---
+++ module.health.rb
@@ -0,0 +1,44 @@
+#!/bin/env ruby
+# encoding: utf-8
+
+class Answer
+
+    # Available: moduleName,methodName,username,message
+
+    def health
+
+        @memory.connect()
+        thoughts = @memory.load("health #{@username}")
+
+        health = thoughts[2]
+
+        if health < 1
+            return "@#{@username}: You are _dead_."
+        else
+            return "@#{@username}: You currently have #{health}hp."
+        end
+
+    end
+
+    def all
+
+        @memory.connect()
+        thoughts = @memory.load("health ")
+
+        standings = ""
+        thoughts.each do |known|
+            if known[0] != "ludivine" then next end
+            if known[1].split(" ")[0] != "health" then next end
+            if known[2].to_i < 1
+                standings += "*" + known[1].sub("health", "") + " * _dead_\n"
+            else
+                standings += "*" + known[1].sub("health", "") + "* " + known[2] + "hp\n"
+            end
+
+        end
+
+        return "Current health standings:\n#{standings}"
+
+    end
+
+end
