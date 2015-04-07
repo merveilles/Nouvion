cf036b2daded87e7c5a0b6a807e655a6bcf449fe
Devine Lu Linvega <aliceffekt@gmail.com>
2015-04-07 10:23:09 -0400

    Improved the who module

--- module.who.rb
+++ module.who.rb
@@ -13,22 +13,22 @@ class Answer
         twitter = ""
         collaborations = 0
         collaborationsString = ""
+        location = ""
 
         thoughts.each do |known|
-
-            # Definition
             if known[1] == username then definition = known[2] end
-            if known[0] == username && known[1] == "my twitter" then twitter = "And <https://twitter.com/"+known[2]+"|@"+known[2]+"> on Twitter. " end
+            if known[0] == username && known[1] == "my twitter" then twitter   = "This user's Twitter is <https://twitter.com/"+known[2]+"|@"+known[2]+">. " end
+            if known[0] == username && known[1] == "my location" then location = "Currently located in *"+known[2]+"*. " end
             if known[0] == username then collaborations += 1 end
         end
 
         if collaborations > 0
-            collaborationsString = "*"+username+"* has added *#{collaborations} entries* to my memory."
+            collaborationsString = "\n*"+username.capitalize+"* has added *#{collaborations} entries* to my memory. "
         end
 
         if definition == "" then return "I do not know *#{username}*." end
 
-        return "*"+username+"* is "+definition+". "+twitter+collaborationsString
+        return "*"+username.capitalize+"* is "+definition+". \n"+location+twitter+collaborationsString
     end
 
     def am
