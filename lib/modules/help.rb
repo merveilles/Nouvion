dadb79967310cad7ad0001e7ac29aaae5aeffb95
Marshall Bowers <elliott.codes@gmail.com>
2015-04-09 23:39:49 -0400

    Port help module

--- lib/modules/help.rb
+++ lib/modules/help.rb
@@ -1,11 +1,6 @@
-#!/bin/env ruby
-# encoding: utf-8
-
 class Answer
-    # Available: moduleName,methodName,username,message
-
     def help
-        'I am bot created in *Ruby* by the Merveilles community, in the <#the-artificial-lounge>. You can ask me for <help> or improve my responses by creating pull requests on Github: https://github.com/merveilles/ludivine'
+        'I am bot created in *Ruby* by the Merveilles community, in the <#the-artificial-lounge>. You can ask me for <help> or improve my responses by creating pull requests on Github: https://github.com/merveilles/nouvion'
     end
 
     def channels
@@ -15,8 +10,8 @@ class Answer
     def modules
         visible_modules = ''
 
-        Dir.entries('.').each do |name, _v|
-            unless name.include?('module.') then next end
+        Dir.entries('lib/modules').each do |name, _v|
+            unless name.include?('.rb') then next end
 
             visible_modules += '`' + name.sub('module.', '').sub('.rb', '') + '` '
         end
