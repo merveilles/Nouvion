067b31bb84fb0e30018750fbc6efa31695f08bcc
Marshall Bowers <elliott.codes@gmail.com>
2015-04-09 22:52:27 -0400

    Port ruby module

---
+++ lib/modules/ruby.rb
@@ -0,0 +1,13 @@
+class Answer
+    def ruby
+        return "I currently run on Ruby *#{RUBY_VERSION}*."
+    end
+
+    def version
+        return "I currently run on Ruby *#{RUBY_VERSION}*."
+    end
+
+    def v
+        return version
+    end
+end
