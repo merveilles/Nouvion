f8923822437f60e82cbaaccbc4ea19558be66b2b
Marshall Bowers <elliott.codes@gmail.com>
2015-04-10 10:07:57 -0400

    Add more Slack Users API methods

--- lib/slack/users.rb
+++ lib/slack/users.rb
@@ -1,4 +1,63 @@
 module Slack::Users
+    def self.get_presence(user)
+        uri = URI.parse('https://slack.com/api/users.getPresence')
+
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+
+        request = Net::HTTP::Post.new(uri.path)
+
+        params = {
+            'token' => Slack.token,
+            'user' => user
+        }
+
+        request.body = URI.encode_www_form(params)
+
+        response = https.request(request)
+
+        return JSON.parse(response.body)
+    end
+
+    def self.info(user)
+        uri = URI.parse('https://slack.com/api/users.getPresence')
+
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+
+        request = Net::HTTP::Post.new(uri.path)
+
+        params = {
+            'token' => Slack.token,
+            'user' => user
+        }
+
+        request.body = URI.encode_www_form(params)
+
+        response = https.request(request)
+
+        return JSON.parse(response.body)
+    end
+
+    def self.list
+        uri = URI.parse('https://slack.com/api/users.getPresence')
+
+        https = Net::HTTP.new(uri.host, uri.port)
+        https.use_ssl = true
+
+        request = Net::HTTP::Post.new(uri.path)
+
+        params = {
+            'token' => Slack.token
+        }
+
+        request.body = URI.encode_www_form(params)
+
+        response = https.request(request)
+
+        return JSON.parse(response.body)
+    end
+
     def self.set_presence(presence)
         uri = URI.parse('https://slack.com/api/users.setPresence')
 
