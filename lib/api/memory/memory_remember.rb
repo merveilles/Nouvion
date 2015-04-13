ee444e9336c07b4adf7806b692111bbde97fd96a
Marshall Bowers <elliott.codes@gmail.com>
2015-04-13 10:22:56 -0400

    Improve remember memory

--- lib/api/memory/memory_remember.rb
+++ lib/api/memory/memory_remember.rb
@@ -18,7 +18,15 @@ module API
             @db.execute(table)
         end
 
-        def save(username, term, definition, relation)
+        def exists(username, term)
+            return @db.execute('
+                SELECT * FROM remember
+                WHERE username = ?
+                AND term = ?
+            ', [username, term]).length > 0
+        end
+
+        def add(username, term, definition, relation)
             @db.execute('
                 INSERT INTO remember (username, term, definition, relation)
                 VALUES (?, ?, ?, ?)
@@ -26,10 +34,49 @@ module API
         end
 
         def load(term)
+            return load_by_term(term)
+        end
+
+        def load_by_term(term)
+            return @db.execute('
+                SELECT * FROM remember
+                WHERE term = ?
+            ', [term])
+        end
+
+        def load_by_definition(definition)
             return @db.execute('
                 SELECT * FROM remember
                 WHERE definition = ?
+            ', [definition])
+        end
+
+        def update(username, term, definition, relation)
+            puts 'do an update'
+            @db.execute('
+                UPDATE remember
+                SET username = ?, term = ?, definition = ?, relation = ?
+                WHERE username = ?
+                    AND term = ?
+            ', [username, term, definition, relation, username, term])
+        end
+
+        def delete(term)
+            return delete_by_term(term)
+        end
+
+        def delete_by_term(term)
+            return @db.execute('
+                DELETE FROM remember
+                WHERE term = ?
             ', [term])
+        end
+
+        def delete_by_definition
+            return @db.execute('
+                DELETE FROM remember
+                WHERE definition = ?
+            ', [definition])
         end
     end
 end
--- lib/api/remember.rb
+++ lib/api/remember.rb
@@ -8,7 +8,11 @@ module API
         end
 
         def store(term, definition, relation)
-            @memory.save(@username, term, definition, relation)
+            if @memory.exists(@username, term)
+                @memory.update(@username, term, definition, relation)
+            else
+                @memory.add(@username, term, definition, relation)
+            end
         end
 
         def recall(term)
