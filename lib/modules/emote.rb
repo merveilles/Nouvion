ac6f561e55bd5dc09449998daf0ce89a00f7bfc1
Marshall Bowers <elliott.codes@gmail.com>
2015-04-13 12:06:09 -0400

    Port emote module
    Remove memory integration.

---
+++ lib/modules/emote.rb
@@ -0,0 +1,56 @@
+class Answer
+    def emote
+        smile
+    end
+
+    def smile
+        oper = @message.split(' ')[0]
+        val1 = @message.split(' ')[1].to_i
+        val2 = @message.split(' ')[2].to_i
+
+        eyes = ['O', 'o', '^', '*']
+        mouths = ['_', '__', '.']
+
+        symmetry = rand(1) > 0
+
+        lefteye = eyes.shuffle[0]
+        righteye = lefteye
+        unless  symmetry
+            righteye = eyes.shuffle[0]
+        end
+
+        text = lefteye + mouths.shuffle[0] + righteye
+
+        text
+    end
+
+    def flip
+        @flip_emojis = ['(ﾉ °□°)ﾉ︵ ┻─┻', '(ノ ゜Д゜)ノ ︵ ┻━┻', '(ﾉಥ益ಥ）ﾉ﻿ ┻━┻', "(╯'□')╯︵ ┻━┻"]
+
+        message = @message.sub('emote flip', '').strip
+
+        return (@flip_emojis.sample + if message.length > 0 then " *#{message}* " else '' end)
+    end
+
+    def shrug
+        message = @message.sub('emote shrug', '').strip
+
+        return (if message.length > 0 then "*#{message}* " else '' end + '¯\\_(ツ)_/¯')
+    end
+
+    def donger
+        message = @message.sub('emote donger', '').strip
+
+        message = if message.length > 0 then message else 'raise your dongers' end
+
+        return "ヽ༼ຈل͜ຈ༽ﾉ #{message} ヽ༼ຈل͜ຈ༽ﾉ"
+    end
+
+    def molly
+        message = @message.sub('emote molly', '').strip
+
+        message = if message.length > 0 then message.upcase else 'MOLLY' end
+
+        return "༼ つ ◕_◕ ༽つ #{message} ༼ つ ◕_◕ ༽つ"
+    end
+end
