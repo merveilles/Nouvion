178e3a36813cc66584e7c8b0ef4946a60bdbfedc
Philip Karpiak <operations@eswat.ca>
2015-04-15 10:54:24 -0400

    GET OVER HERE: add mkx method to lorem module

--- lib/modules/lorem.rb
+++ lib/modules/lorem.rb
@@ -1,6 +1,6 @@
 class Answer
     def lorem
-        "My lorem module requires a method, and optionally the # of sentences, from this list:\n`gibson` `stephenson` | `gibson 3` `stephenson 4`."
+        "My lorem module requires a method, and optionally the # of sentences, from this list:\n`gibson` `stephenson` `mkx` | `gibson 3` `stephenson 4`."
     end
 
     def gibson
@@ -14,6 +14,15 @@ class Answer
 
     def stephenson
         words = ['pizza', 'metaverse', 'shadowy', 'virtual', 'smartwheel', 'reason', 'rat thing', 'drug lord', 'corporation', 'entrepreneur', 'oreo', 'vapor of nuance', 'sintered armorgel', 'katana', 'dentata', 'mafia ethic', 'metacop', 'post-modern', 'caucasians with guns', 'nipponese', 'warrior prince', 'substance', 'unauthorized', 'viruses', 'infected', 'software', 'mass hysteria', 'techno-shit', 'xerox', 'business plan', 'arachnofiber weave', 'loogie gun', 'u-stor-it', 'magnapoon', 'hypercard', 'avatar', 'airbag jacket', 'virtual earth']
+
+        sentenceCount = @message.split(' ')[2].to_i.abs
+        sentence = text(sentenceCount, words)
+
+        return sentence
+    end
+
+    def mkx
+        words = ["spec ops", "hollywood", "brawler", "brood mother", "swarm queen", "venomous", "spectral", "mystic", "master of souls", "marksman", "gunslinger", "outlaw", "ruthless", "vicious", "lackey", "tigrar fury", "kuatan warrior", "dragon fangs", "shotgun", "full auto", "high tech", "stalker", "undead", "uber", "pumped up", "heavy weapons", "wrestler", "a-list", "fisticuffs", "stunt double", "commando", "cutthroat", "cybernetic", "possessed", "balanced", "kenjutsu", "mournful", "royal storm", "assassin", "sun god", "blood god", "war god", "ancestral", "bojutsu", "shaolin", "hat trick", "tempest", "buzz saw", "flame fist", "dragon's fire", "dualist", "ethereal", "piercing", "ravenous", "hunter", "trophy collector", "extraterrestrial", "summoner", "sorcerer", "warlock", "master of storms", "thunder god", "displacer", "noxious", "deceptive", "nimble", "inferno", "ninjutsu", "hellfire", "impostor", "bone shaper", "necromancer", "covert ops", "demolition", "special forces", "grandmaster", "cryomancer", "unbreakable", "shirai ryu", "lasher", "ronin", "deceiver", "edenian guard", "manipulator", "copycat", "elemental", "fugitive"]
 
         sentenceCount = @message.split(' ')[2].to_i.abs
         sentence = text(sentenceCount, words)
