216d86459842a60dfc45b6bba305044d6b0f1803
Marshall Bowers <elliott.codes@gmail.com>
2015-04-20 10:06:53 -0400

    Update tell tests

--- test/test_tell.rb
+++ test/test_tell.rb
@@ -1,33 +1,29 @@
-#!/bin/env ruby
-# encoding: utf-8
-
 require 'coveralls'
 Coveralls.wear!
 
 require 'minitest/autorun'
-require 'memory'
-require_relative '../answer'
+require_relative '../lib/modules/answer'
 
 class TestTell < Minitest::Test
     def setup
-        require_relative '../module.tell'
+        require_relative '../lib/modules/tell'
     end
 
     def test_tell_tell
-        answer = Answer.new('tell', 'tell', 'maxdeviant', 'tell', 'theartificiallounge')
+        answer = Answer.new('tell', 'tell', 'maxdeviant', 'theartificiallounge', 'tell')
 
         tell = answer.tell
 
-        assert_equal "I don't know what to tell you.", tell
+        assert_equal "<@maxdeviant>: I don't know what to tell you.", tell
 
         messages = [
-          'tell me something dark',
-          'rules',
-          'manifesto'
+            'tell me something dark',
+            'rules',
+            'manifesto'
         ]
 
         messages.each do |message|
-            answer = Answer.new('tell', 'tell', 'maxdeviant', message, 'theartificiallounge')
+            answer = Answer.new('tell', 'tell', 'maxdeviant', 'theartificiallounge', message)
 
             tell = answer.tell
 
