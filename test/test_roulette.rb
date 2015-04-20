93df56285d030ba6f1891c89f28ae17e7d52e213
Marshall Bowers <elliott.codes@gmail.com>
2015-04-20 10:05:34 -0400

    Update roulette tests

--- test/test_roulette.rb
+++ test/test_roulette.rb
@@ -1,22 +1,17 @@
-#!/bin/env ruby
-# encoding: utf-8
-
-require "coveralls"
+require 'coveralls'
 Coveralls.wear!
 
-require "minitest/autorun"
-require "api"
-require_relative "../answer"
+require 'minitest/autorun'
+require 'api'
+require_relative '../lib/modules/answer'
 
 class TestRoulette < Minitest::Test
     def setup
-        require_relative "../module.roulette"
-
-        @@memory = RouletteTestMemory.new
+        require_relative '../lib/modules/roulette'
     end
 
     def test_roulette_roulette
-        answer = Answer.new("roulette", "roulette", "maxdeviant", "roulette", "theartificiallounge")
+        answer = Answer.new('roulette', 'roulette', 'maxdeviant', 'theartificiallounge', 'roulette')
 
         roulette = answer.roulette
 
@@ -24,53 +19,27 @@ class TestRoulette < Minitest::Test
     end
 
     def test_roulette_load
-        answer = Answer.new("roulette", "load", "maxdeviant", "roulette load", "theartificiallounge", @@memory)
+        answer = Answer.new('roulette', 'load', 'maxdeviant', 'theartificiallounge', 'roulette load')
 
         load = answer.load
 
-        assert_equal "@maxdeviant: You put a bullet in the revolver. :japanese_ogre:", load
+        assert_equal true, load.include?('a bullet in the revolver.')
 
     end
 
     def test_roulette_spin
-        answer = Answer.new("roulette", "spin", "maxdeviant", "roulette spin", "theartificiallounge", @@memory)
+        answer = Answer.new('roulette', 'spin', 'maxdeviant', 'theartificiallounge', 'roulette spin')
 
         spin = answer.spin
 
-        assert_equal "maxdeviant spins the cylinder. :return:", spin
+        assert_equal '<@maxdeviant> spins the cylinder. :return:', spin
     end
 
     def test_roulette_pull
-        answer = Answer.new("roulette", "pull", "maxdeviant", "roulette pull", "theartificiallounge", @@memory)
+        answer = Answer.new('roulette', 'pull', 'maxdeviant', 'theartificiallounge', 'roulette pull')
 
         pull = answer.pull
 
         assert_equal true, pull.instance_of?(String)
-    end
-end
-
-class RouletteTestMemory < Memory
-    attr_reader :memory
-
-    def initialize
-        @memory = [
-            ["ludivine", "health maxdeviant", "7"],
-            ["ludivine", "health aliceffekt", "0"],
-            ["ludivine", "roulette chamber", "000000"]
-        ]
-    end
-
-    def connect
-
-    end
-
-    def save(owner, key, value)
-        if key == "roulette chamber"
-            @memory[2][2] = value
-        end
-    end
-
-    def load(query)
-        @memory.select { |m| m[1].include? query }
     end
 end
