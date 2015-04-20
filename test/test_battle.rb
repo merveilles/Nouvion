a01bd92f5893e6a56569bbc7b6ee1d59740bcef7
Marshall Bowers <elliott.codes@gmail.com>
2015-04-20 10:11:33 -0400

    Update battle tests (broken)

--- test/test_battle.rb
+++ test/test_battle.rb
@@ -1,90 +1,55 @@
-#!/bin/env ruby
-# encoding: utf-8
-
 require 'coveralls'
 Coveralls.wear!
 
 require 'minitest/autorun'
-require 'memory'
-require_relative '../answer'
+require_relative '../lib/modules/answer'
 
 class TestBattle < Minitest::Test
     def setup
-        require_relative '../module.battle'
+        require_relative '../lib/modules/battle'
     end
 
     def test_battle_profession
-        mem = BattleMemoryTest.new
+        answer = Answer.new('battle', 'profession', 'strstr', 'theartificiallounge', 'battle profession')
 
-        answer = Answer.new('battle', 'profession', 'strstr', 'battle profession', 'theartificiallounge', mem)
         battle = answer.profession
-        # puts battle #todo : assert that the messages are correct by exposing their result somehow
 
-        answer = Answer.new('battle', 'profession', 'strstr', 'battle profession rogue', 'theartificiallounge', mem)
-        battle = answer.profession
-        # puts battle
+        answer = Answer.new('battle', 'profession', 'strstr', 'theartificiallounge', 'battle profession rogue')
 
-        answer = Answer.new('battle', 'profession', 'strstr', 'battle profession', 'theartificiallounge', mem)
         battle = answer.profession
-        # puts battle
 
-        answer = Answer.new('battle', 'profession', 'strstr', 'battle profession rogue', 'theartificiallounge', mem)
-        battle = answer.profession
-        # puts battle
+        answer = Answer.new('battle', 'profession', 'strstr', 'theartificiallounge', 'battle profession')
 
-        answer = Answer.new('battle', 'profession', 'strstr', 'battle profession potato', 'theartificiallounge', mem)
         battle = answer.profession
-      # puts battle
+
+        answer = Answer.new('battle', 'profession', 'strstr', 'theartificiallounge', 'battle profession rogue')
+
+        battle = answer.profession
+
+        answer = Answer.new('battle', 'profession', 'strstr', 'theartificiallounge', 'battle profession potato')
+
+        battle = answer.profession
     end
 
     def test_battle_attack
-        mem = BattleMemoryTest.new
+        answer = Answer.new('battle', 'attack', 'strstr', 'theartificiallounge', 'battle attack potato')
 
-        answer = Answer.new('battle', 'attack', 'strstr', 'battle attack potato', 'theartificiallounge', mem)
         battle = answer.attack
-        # puts battle # manual validation for now until I expose the battle messages
 
-        answer = Answer.new('battle', 'profession', 'strstr', 'battle profession rogue', 'theartificiallounge', mem)
+        answer = Answer.new('battle', 'profession', 'strstr', 'theartificiallounge', 'battle profession rogue')
+
         battle = answer.profession
-        # puts battle
 
-        answer = Answer.new('battle', 'attack', 'strstr', 'battle attack potato', 'theartificiallounge', mem)
+        answer = Answer.new('battle', 'attack', 'strstr', 'theartificiallounge', 'battle attack potato')
+
         battle = answer.attack
-        # puts battle
 
-        answer = Answer.new('battle', 'raise', 'strstr', 'battle raise strstr', 'theartificiallounge', mem)
+        answer = Answer.new('battle', 'raise', 'strstr', 'theartificiallounge', 'battle raise strstr')
+
         battle = answer.raise
-        # puts battle
 
-        answer = Answer.new('battle', 'raise', 'strstr', 'battle raise potato', 'theartificiallounge', mem)
+        answer = Answer.new('battle', 'raise', 'strstr', 'theartificiallounge', 'battle raise potato')
+
         battle = answer.raise
-      # puts battle
-    end
-end
-
-class BattleMemoryTest < Memory
-    attr_reader :mem
-    def initialize
-      @mem = [%w(ludivine potato no),
-              ['ludivine', 'profession strstr', 'mage'],
-              ['ludivine', 'health strstr', '7'],
-              %w(orange seeds yes),
-              ['ludivine', 'health potato', '2']]
-    end
-
-    def load(_query)
-        @mem
-    end
-
-    def save(owner, key, value)
-        if key == 'profession strstr' then
-           @mem[1][2] = value
-           return
-        end
-
-        if owner == 'ludivine' && key == 'health potato' then
-           @mem[4][2] = value
-           return
-        end
     end
 end
