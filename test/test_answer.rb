fb19b564d4cfd8348e43c6a59637b65eabda7e28
Marshall Bowers <elliott.codes@gmail.com>
2015-04-20 09:19:09 -0400

    Update answer tests

--- test/test_answer.rb
+++ test/test_answer.rb
@@ -1,51 +1,47 @@
-#!/bin/env ruby
-# encoding: utf-8
-
 require 'coveralls'
 Coveralls.wear!
 
 require 'minitest/autorun'
-require 'memory'
-require_relative '../answer'
+require_relative '../lib/modules/answer'
 
 class TestAnswer < Minitest::Test
     def setup
     end
 
     def test_answer_hey
-        answer = Answer.new('answer', 'hey', 'maxdeviant', 'hey', 'theartificiallounge')
+        answer = Answer.new('answer', 'hey', 'maxdeviant', 'theartificiallounge', 'hey')
 
         hey = answer.hey
 
-        assert_equal 'hey maxdeviant.', hey
+        assert_equal 'Hey <@maxdeviant>.', hey
     end
 
     def test_answer_hi
-        answer = Answer.new('answer', 'hi', 'maxdeviant', 'hi', 'theartificiallounge')
+        answer = Answer.new('answer', 'hi', 'maxdeviant', 'theartificiallounge', 'hi')
 
         hi = answer.hi
 
-        assert_equal 'hi maxdeviant!', hi
+        assert_equal 'Hi <@maxdeviant>!', hi
     end
 
     def test_answer_sorry
-        answer = Answer.new('answer', 'sorry', 'maxdeviant', 'sorry', 'theartificiallounge')
+        answer = Answer.new('answer', 'sorry', 'maxdeviant', 'theartificiallounge', 'sorry')
 
         sorry = answer.sorry
 
-        assert_equal "Don't worry about it *maxdeviant*.", sorry
+        assert_equal "Don't worry about it <@maxdeviant>.", sorry
     end
 
     def test_answer_where
-        answer = Answer.new('answer', 'where', 'maxdeviant', 'where', 'theartificiallounge')
+        answer = Answer.new('answer', 'where', 'maxdeviant', 'theartificiallounge', 'where')
 
         where = answer.where
 
-        assert_equal 'You are in *theartificiallounge*.', where
+        assert_equal 'You are in <#theartificiallounge>.', where
     end
 
     def test_answer_rules
-        answer = Answer.new('answer', 'rules', 'maxdeviant', 'rules', 'theartificiallounge')
+        answer = Answer.new('answer', 'rules', 'maxdeviant', 'theartificiallounge', 'rules')
 
         rules = answer.rules
 
