2caf1da804f435bbc10e167b3376468d31702326
Marshall Bowers <elliott.codes@gmail.com>
2015-04-20 09:59:56 -0400

    Update sing tests

--- test/test_sing.rb
+++ test/test_sing.rb
@@ -1,15 +1,12 @@
-#!/bin/env ruby
-# encoding: utf-8
-
 require 'coveralls'
 Coveralls.wear!
 
 require 'minitest/autorun'
-require_relative '../answer'
+require_relative '../lib/modules/answer'
 
 class TestSing < Minitest::Test
 	def setup
-	    require_relative '../module.sing'
+	    require_relative '../lib/modules/sing'
 	end
 
 	def test_sing
@@ -17,7 +14,7 @@ class TestSing < Minitest::Test
 
 		artist.each do|n|
 
-	        answer = Answer.new('sing', 'sing', 'ragekit', 'sing ' + n, 'theartificiallounge')
+	        answer = Answer.new('sing', 'sing', 'ragekit', 'theartificiallounge', 'sing ' + n,)
 
 	        sing = answer.sing
 	        assert_equal true, (sing.instance_of? String)
